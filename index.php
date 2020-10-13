8b4f4ec75aebc5cef0a67506cf5a9401336d16ca
webmac <macuri516@gmail.com>
2020-10-13 00:53:27 -0500

    comit 01 origen

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>git orgrgen</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h2>titulo de cabezera</h2>
+	<p>esto es el archivo origen</p>
+	<p>comit1 de origen</p>
+</body>
+</html>
